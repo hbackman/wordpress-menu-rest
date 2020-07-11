5eaba5d3b3f0b269350b850ae3bc7d9c37417716
Hampus Backman <hampus@safetyamp.com>
2020-07-11 15:06:29 -0400

    Removed debug code

--- wordpress-menu-rest.php
+++ wordpress-menu-rest.php
@@ -10,10 +10,6 @@ Author URI: http://hbackman.se
 
 define('WORDPRESS_MENU_REST_VERSION', '1.0');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 function getMenu(WP_REST_Request $request) {
     // Attempt to get all menus matching the
     // given slug in the request
